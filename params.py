3495fae885a0e632d58b940b67ee200ea7a62307
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2022-08-12 22:12:49 +0200

    tested for increasing number of params

--- params.py
+++ params.py
@@ -5,8 +5,8 @@ import time
 def set_data_params(apiParams, posArgs):
     """
     A function to map the positional arguments to the API parameters
-    :param apiParams: list, the API parameters
-    :param posArgs: list, the positional parameters
+    :param apiParams: tuple, the API parameters
+    :param posArgs: tuple, the positional parameters
     :return: dict
     """
     dataDict = dict()
@@ -33,6 +33,27 @@ def set_args():
         parser.add_argument('--five', type=float, help='five', required=False)
         parser.add_argument('--six', type=set, help='six REQUIRED', required=True)
         parser.add_argument('--seven', type=int, help='seven', required=False)
+        # parser.add_argument('--one1', type=int, help='first REQUIRED', required=True)
+        # parser.add_argument('--two1', type=str, help='two REQUIRED', required=True)
+        # parser.add_argument('--three1', type=list, help='three REQUIRED', required=False)
+        # parser.add_argument('--four1', type=tuple, help='four REQUIRED', required=True)
+        # parser.add_argument('--five1', type=float, help='five', required=False)
+        # parser.add_argument('--six1', type=set, help='six REQUIRED', required=True)
+        # parser.add_argument('--seven1', type=int, help='seven', required=False)
+        # parser.add_argument('--one2', type=int, help='first REQUIRED', required=True)
+        # parser.add_argument('--two2', type=str, help='two REQUIRED', required=True)
+        # parser.add_argument('--three2', type=list, help='three REQUIRED', required=False)
+        # parser.add_argument('--four2', type=tuple, help='four REQUIRED', required=True)
+        # parser.add_argument('--five2', type=float, help='five', required=False)
+        # parser.add_argument('--six2', type=set, help='six REQUIRED', required=True)
+        # parser.add_argument('--seven2', type=int, help='seven', required=False)
+        # parser.add_argument('--one3', type=int, help='first REQUIRED', required=True)
+        # parser.add_argument('--two3', type=str, help='two REQUIRED', required=True)
+        # parser.add_argument('--three3', type=list, help='three REQUIRED', required=False)
+        # parser.add_argument('--four3', type=tuple, help='four REQUIRED', required=True)
+        # parser.add_argument('--five3', type=float, help='five', required=False)
+        # parser.add_argument('--six3', type=set, help='six REQUIRED', required=True)
+        # parser.add_argument('--seven3', type=int, help='seven', required=False)
 
         return parser.parse_args()
     except Exception as e:
--- paramsmain.py
+++ paramsmain.py
@@ -4,7 +4,17 @@ from params import set_args, set_data_params
 if __name__ == '__main__':
     args = set_args()
     api_params = ('oNe', 'tWo', 'tHrEe', 'fOuR', 'fIvE', 'sIx', 'sEvEn')
+    # api_params = ('oNe', 'tWo', 'tHrEe', 'fOuR', 'fIvE', 'sIx', 'sEvEn',
+    #               'oNe1', 'tWo1', 'tHrEe1', 'fOuR1', 'fIvE1', 'sIx1', 'sEvEn1',
+    #               'oNe2', 'tWo2', 'tHrEe2', 'fOuR2', 'fIvE2', 'sIx2', 'sEvEn2',
+    #               'oNe3', 'tWo3', 'tHrEe3', 'fOuR3', 'fIvE3', 'sIx3', 'sEvEn3'
+    #               )
     positional_params = (args.one, args.two, args.three, args.four, args.five, args.six, args.seven)
+    # positional_params = (args.one, args.two, args.three, args.four, args.five, args.six, args.seven,
+    #                      args.one1, args.two1, args.three1, args.four1, args.five1, args.six1, args.seven1,
+    #                      args.one2, args.two2, args.three2, args.four2, args.five2, args.six2, args.seven2,
+    #                      args.one3, args.two3, args.three3, args.four3, args.five3, args.six3, args.seven3
+    #                      )
 
     # with the builtin function
     start = time.perf_counter()
