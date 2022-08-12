347175eca9e9a092ac6efa38e788da5ff22d1c02
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2022-08-12 20:38:57 +0200

    no more args list slicing yay ^^

--- paramsmain.py
+++ paramsmain.py
@@ -3,5 +3,6 @@ from params import set_args, set_data_params
 if __name__ == '__main__':
     args = set_args()
     api_params = ['oNe', 'tWo', 'tHrEe', 'fOuR', 'fIvE', 'sIx', 'sEvEn']
-    data = set_data_params(api_params, list(vars(args).values()))
+    positional_params = [args.one, args.two, args.three, args.four, args.five, args.six, args.seven]
+    data = set_data_params(api_params, positional_params)
     print(data)
